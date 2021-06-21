bd496b3159a0860417a0d5ce2c107ae86da2d4db
Brittany Johnson <brittanyjohnson16@yahoo.com>
2021-06-21 18:18:24 -0500

    make sure data processing works

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,23 @@
+function genPlots(id) {
+    d3.json("data/samples.json").then((data)=> {
+        console.log(data)
+        //get top 10
+        var samples = data.samples.filter(s => s.id.toString() === id)[0];
+        console.log(samples);
+        var samplevalues = samples.sample_values.slice(0, 10).reverse();
+  
+        var OTU_top = (samples.otu_ids.slice(0, 10)).reverse();
+        
+        // get the otu id's to the desired form for the plot
+        var OTU_id = OTU_top.map(d => "OTU " + d)
+  
+        console.log(`OTU IDS: ${OTU_id}`)
+        console.log(`samplevalues: ${samplevalues}`)
+  
+  
+    })
+}
+
 function getInfo(id) {
     d3.json("data/samples.json").then((data)=> {
 
@@ -30,6 +50,7 @@ function initialize_page() {
         data.names.forEach(function(name) {
             dropdown.append("option").text(name).property("value");
         });
+        genPlots(data.names[0])
         getInfo(data.names[0])
     });
 
