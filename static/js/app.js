da4dead98a1e0fc60c34b66d1c3754c6f27c74d2
Brittany Johnson <brittanyjohnson16@yahoo.com>
2021-06-21 18:00:40 -0500

    try to add the demographic info as well

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,22 @@
-  
+function getInfo(id) {
+    d3.json("data/samples.json").then((data)=> {
+
+        var metadata = data.metadata;
+        console.log(metadata)
+        // filter by id
+        var result = metadata.filter(meta => meta.id.toString() === id)[0];
+
+        var demographicInfo = d3.select("#sample-metadata");
+        
+        // refresh demographic panel
+        demographicInfo.html("");
+
+        // grab the demographic data
+        Object.entries(result).forEach((key) => {   
+                demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");    
+        });
+    });
+}  
 function initialize_page() {
     var dropdown = d3.select("#selDataset");
 
@@ -8,6 +26,8 @@ function initialize_page() {
             dropdown.append("option").text(name).property("value");
         });
     });
+
+    getInfo(data.names[0])
 }
 
 initialize_page();
