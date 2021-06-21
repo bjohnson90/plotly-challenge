744cf7602187c0b79ecbe6ce983014cb2681ab82
Brittany Johnson <brittanyjohnson16@yahoo.com>
2021-06-21 18:01:40 -0500

    capitalized data folder by mistake

--- static/js/app.js
+++ static/js/app.js
@@ -20,7 +20,7 @@ function getInfo(id) {
 function initialize_page() {
     var dropdown = d3.select("#selDataset");
 
-    d3.json("Data/samples.json").then((data)=> {
+    d3.json("data/samples.json").then((data)=> {
         console.log(data)
         data.names.forEach(function(name) {
             dropdown.append("option").text(name).property("value");
